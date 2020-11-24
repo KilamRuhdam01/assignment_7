788be9cdfd64a21a492b145c16c0c62d9ac11e93
Madhur <eet202757@iitd.ac.in>
2020-11-24 15:09:26 +0530

    Checked and completed ps1

--- ps_1.py
+++ ps_1.py
@@ -23,13 +23,24 @@ class Lab:
 
 class Software_lab(Lab):
     def set_marks(self,marks):
-        self.marks=marks
+        if marks >=0:
+            self.marks=marks
+        else:
+            print('Wrong marks entered, they cant be negative')
     def p_details(self):
         if super().get_role() == 'I':
-            print('\n{} can be reached at {}'.format(super().get_name(),super().get_email()))
+            print('\n{}({}) can be reached at {}\n'.format(super().get_name(),super().get_role(),super().get_email()))
+        elif super().get_role() == 'S':
+            print('\n{} got {} marks\n'.format(super().get_name(), str(self.marks)))
+        else:
+            print('\nThe role is incorrect please correct it using set_role() method. \n')
 
+subrat = Software_lab('Subrat','subrat@iit','I')
+subrat.p_details()
+madhur = Software_lab('Madhur', 'eet2757@iit', 'S')
+madhur.set_marks(99)
+madhur.p_details()
 
-Subrat = Software_lab('Subrat','subrat@iit','I')
-Subrat.p_details()
-
+sonal = Software_lab('Sonal', 'eet2757@iit', 'T')
+sonal.p_details()
 
